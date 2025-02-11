2a16ab9b62c61af124d232cf887896381489ee22
Christopher Edwin Mtoi <135335764+Chriss875@users.noreply.github.com>
2025-02-11 12:41:44 +0300

    TESTS

---
+++ nmbscholarshipmanagementsystem/src/test/java/com/nmb/admin/servicetest/UserServiceTest.java
@@ -0,0 +1,11 @@
+package com.nmb.admin.servicetest;
+
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@RunWith(MockitoJUnitRunner.class)
+public class UserServiceTest {
+    
+    
+}
